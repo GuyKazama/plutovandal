2ff0ee9a76d642686f5f31872dbaefc2cb7a0fc7
Max Alford <blackmountainmax@gmail.com>
2020-08-11 01:00:43 -0400

    simple decal painting system
    press E to paint on surfaces wherever your cursor is. Not perfect, but ya know
    also moved scripts and prefabs to their own folders.
    weird GUI error keeps popping up, dunno what that's about. I'll look into it soon.

---
+++ Assets/scripts/raycastpaint.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class raycastpaint : MonoBehaviour
+{
+    public GameObject Paint;
+    public GameObject Spraycan;
+    
+    
+    
+    // See Order of Execution for Event Functions for information on FixedUpdate() and Update() related to physics queries
+    void Update()
+    {
+        if (Input.GetKey("e"))
+        {
+            //shorthand so I don't need to write that out everytime
+            //Vector3 fwd = transform.TransformDirection(Vector3.forward);
+            //Debug.DrawLine(Vector3.zero, Vector3.forward * 100);
+            Ray rayOrigin = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            //draws line 10 units in front of player
+            if (Physics.Raycast(rayOrigin, out hitInfo))
+            {
+                if (hitInfo.collider.tag == "taggable surface")
+                {
+                    Instantiate(Paint, hitInfo.point, Quaternion.LookRotation(hitInfo.normal));
+                    //print("There is something in front of the object!");
+
+                }
+            }
+        }
+       // if (Physics.Raycast(transform.position, fwd, 10))
+         //   print("There is something in front of the object!");
+    }
+}
