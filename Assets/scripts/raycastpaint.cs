866812836786bf9704839ad801a4ea8556ab5e2e
Max Alford <blackmountainmax@gmail.com>
2020-08-11 23:47:29 -0400

    HOLY PAINTS BATMAN
    painting with different colors!!!!

--- Assets/scripts/raycastpaint.cs
+++ Assets/scripts/raycastpaint.cs
@@ -2,15 +2,56 @@
 
 public class raycastpaint : MonoBehaviour
 {
-    public GameObject Paint;
-    //public GameObject Spraycan;
 
+
+   // public float currentpaint;
+    public GameObject paint;
+
+
+    //public void changepaint(int num)
+    //{
+    //    currentpaint = num;
+    //    for(int i = 0; i < paints.Length; i++)
+    //    {
+    //        if (i == num)
+    //            paints[i].gameObject.SetActive(true);
+    //        else
+    //            paints[i].gameObject.SetActive(false);
+    //    }
+    //}
 
     void Start()
     {
     }
     void Update()
     {
+
+        if (Input.GetKeyDown("1"))
+        {
+            paint = GameObject.FindGameObjectWithTag("paint_white");
+        }
+
+        if (Input.GetKeyDown("2"))
+        {
+            paint = GameObject.FindGameObjectWithTag("paint_red");
+        }
+
+        if (Input.GetKeyDown("3"))
+        {
+            paint = GameObject.FindGameObjectWithTag("paint_green");
+        }
+
+        if (Input.GetKeyDown("4"))
+        {
+            paint = GameObject.FindGameObjectWithTag("paint_blue");
+        }
+
+        if (Input.GetKeyDown("5"))
+        {
+            paint = GameObject.FindGameObjectWithTag("paint_black");
+        }
+
+
         if (Input.GetKey("e"))
         {
             //spray.SetActive(true);
@@ -25,7 +66,7 @@ public class raycastpaint : MonoBehaviour
                 if (hitInfo.collider.tag == "taggable surface")
                 {
                     //creates gameobject "paint" on whatever surface the ray collided with. hitinfo.normal is the orientation in the world
-                    Instantiate(Paint, hitInfo.point, Quaternion.LookRotation(hitInfo.normal));
+                    Instantiate(paint, hitInfo.point, Quaternion.LookRotation(hitInfo.normal));
                     //print("There is something in front of the object!");
 
                 }
