deece11d5e13b7ba791ed59ab38a03269dd529ae
Max Alford <blackmountainmax@gmail.com>
2020-08-11 23:04:36 -0400

    spraycan update
    choose between colors of spraypaints with the number keys
    also particles

--- Assets/scripts/raycastpaint.cs
+++ Assets/scripts/raycastpaint.cs
@@ -3,19 +3,24 @@
 public class raycastpaint : MonoBehaviour
 {
     public GameObject Paint;
-    public GameObject Spraycan;
- 
+    //public GameObject Spraycan;
 
+
+    void Start()
+    {
+    }
     void Update()
     {
         if (Input.GetKey("e"))
         {
+            //spray.SetActive(true);
             //ray out from the position of the mouse on the screen
             Ray rayOrigin = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hitInfo;
             //draws line 10 units in front of player
             if (Physics.Raycast(rayOrigin, out hitInfo))
             {
+                
                 //maybe could do without this. was following a tutorial, figured it might be nice to have around if we want surfaces that react differently to paint
                 if (hitInfo.collider.tag == "taggable surface")
                 {
@@ -25,11 +30,11 @@ public class raycastpaint : MonoBehaviour
 
                 }
             }
+            
         }
-
-      //  if ()
-       // {
-       //
-       // }
+        //if (Input.GetKeyUp("e"))
+        //{
+        //    spray.SetActive(false);
+        //}
     }
 }
