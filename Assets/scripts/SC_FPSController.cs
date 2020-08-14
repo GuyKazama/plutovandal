1d197ec90e6b3cfe9f38c02834f51325c5688026
Max Alford <blackmountainmax@gmail.com>
2020-08-14 00:31:27 -0400

    movement semi final and new test scene
    simple vertical movement on fps script. Not perfect but more or less what we want. Also added a new scene with space skybox I found on the internet and some test lighting.

--- Assets/scripts/SC_FPSController.cs
+++ Assets/scripts/SC_FPSController.cs
@@ -9,6 +9,7 @@ public class SC_FPSController : MonoBehaviour
     public float walkingSpeed = 7.5f;
     public float runningSpeed = 11.5f;
     public float jumpSpeed = 8.0f;
+    public float fallspeed = -8.0f;
     public float gravity = 0.0f;
     public Camera playerCamera;
     public float lookSpeed = 2.0f;
@@ -42,18 +43,22 @@ public class SC_FPSController : MonoBehaviour
         }
         //Vector3 move_forward = Camera.main.ScreenToViewportPoint(Input.mousePosition);
         // We are grounded, so recalculate move direction based on axes
-        //Vector3 forward = transform.TransformDirection(move_forward);
-        //Vector3 right = transform.TransformDirection(Vector3.right);
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
         // Press Left Shift to run
-       // bool isRunning = Input.GetKey(KeyCode.LeftShift);
-        //float curSpeedX = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Vertical") : 0;
-       // float curSpeedY = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Horizontal") : 0;
-       // float movementDirectionY = moveDirection.y;
-        //moveDirection = (forward * curSpeedX) + (right * curSpeedY) ;
-        //if (Input.GetButton("Jump") && canMove && characterController.isGrounded)
-        //{
-        //    moveDirection.y = jumpSpeed;
-        //}
+        bool isRunning = Input.GetKey(KeyCode.LeftShift);
+        float curSpeedX = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Vertical") : 0;
+        float curSpeedY = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Horizontal") : 0;
+        float movementDirectionY = moveDirection.y;
+        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+        if (Input.GetButton("Jump") )
+        {
+            moveDirection.y = jumpSpeed;
+        }
+        if (Input.GetKey("left shift"))
+        {
+            moveDirection.y = fallspeed;
+        }
         //else
         //{
         //    moveDirection.y = movementDirectionY;
@@ -68,7 +73,7 @@ public class SC_FPSController : MonoBehaviour
         //}
 
         // Move the controller
-        //characterController.Move(moveDirection * Time.deltaTime);
+        characterController.Move(moveDirection * Time.deltaTime);
 
         // Player and Camera rotation
 
