ee764a649fe8d486e821e3e715623997a4a89601
Max Alford <blackmountainmax@gmail.com>
2020-08-29 00:10:26 -0400

    huh

---
+++ Assets/scripts/SprayPaint/Can and Decal/raycastpaint.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+
+public class raycastpaint : MonoBehaviour
+{
+
+
+   // public float currentpaint;
+    public GameObject paint;
+
+
+    //public void changepaint(int num)
+    //{
+    //    currentpaint = num;
+    //    for(int i = 0; i < paints.Length; i++)
+    //    {
+    //        if (i == num)
+    //            paints[i].gameObject.SetActive(true);
+    //        else
+    //            paints[i].gameObject.SetActive(false);
+    //    }
+    //}
+
+    void Start()
+    {
+    }
+    void Update()
+    {
+
+        if (Input.GetKeyDown("1"))
+        {
+            paint = GameObject.FindGameObjectWithTag("paint_white");
+        }
+
+        if (Input.GetKeyDown("2"))
+        {
+            paint = GameObject.FindGameObjectWithTag("paint_red");
+        }
+
+        if (Input.GetKeyDown("3"))
+        {
+            paint = GameObject.FindGameObjectWithTag("paint_green");
+        }
+
+        if (Input.GetKeyDown("4"))
+        {
+            paint = GameObject.FindGameObjectWithTag("paint_blue");
+        }
+
+        if (Input.GetKeyDown("5"))
+        {
+            paint = GameObject.FindGameObjectWithTag("paint_black");
+        }
+
+
+        if (Input.GetMouseButton(0))
+        {
+            //spray.SetActive(true);
+            //ray out from the position of the mouse on the screen
+            Ray rayOrigin = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            //draws line 10 units in front of player
+            if (Physics.Raycast(rayOrigin, out hitInfo))
+            {
+                
+                //maybe could do without this. was following a tutorial, figured it might be nice to have around if we want surfaces that react differently to paint
+                if (hitInfo.collider.tag == "taggable surface")
+                {
+                    //creates gameobject "paint" on whatever surface the ray collided with. hitinfo.normal is the orientation in the world
+                    Instantiate(paint, hitInfo.point, Quaternion.LookRotation(hitInfo.normal));
+                    //print("There is something in front of the object!");
+
+                }
+            }
+            
+        }
+        //if (Input.GetKeyUp("e"))
+        //{
+        //    spray.SetActive(false);
+        //}
+    }
+}
---
+++ Assets/scripts/SprayPaint/Can and Decal/spraycans.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spraycans : MonoBehaviour
+{
+    public int currentcan;
+    public Transform[] cans;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void changecan(int num)
+    {
+        currentcan = num;
+        for(int i = 0; i < cans.Length; i++)
+        {
+            if (i == num)
+                cans[i].gameObject.SetActive(true);
+            else
+                cans[i].gameObject.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("1"))
+        {
+            changecan(0);
+        }
+
+        if (Input.GetKeyDown("2"))
+        {
+            changecan(1);
+        }
+
+        if (Input.GetKeyDown("3"))
+        {
+            changecan(2);
+        }
+
+        if (Input.GetKeyDown("4"))
+        {
+            changecan(3);
+        }
+
+        if (Input.GetKeyDown("5"))
+        {
+            changecan(4);
+        }
+
+    }
+}
